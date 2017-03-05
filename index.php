b6fc81a48b59b8e7ff830efe1ff9de8c0000cb9c
NYCDOE <admin@RM144BWK18.local>
2017-03-05 15:26:15 -0500

    add pgsql
    https://devcenter.heroku.com/articles/heroku-postgresql#connecting-in-php

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
-<?php phpinfo(); 
+<?php 
+require __DIR__ . '/vendor/autoload.php';
+$dbopts = parse_url(getenv('DATABASE_URL'));
+$app->register(new Herrera\Pdo\PdoServiceProvider(),
+               array(
+                   'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"] . ';port=' . $dbopts["port"],
+                   'pdo.username' => $dbopts["user"],
+                   'pdo.password' => $dbopts["pass"]
+               )
+);
