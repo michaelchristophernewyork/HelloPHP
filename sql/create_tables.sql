1011385e33ca69e478c5297e32ec044400251a6c
mcny <michaelchristophernewyork@hotmail.com>
2017-03-28 06:58:25 -0400

    add a new row to the table

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,4 +1,5 @@
 drop table if exists People cascade;
 create table People ( id serial primary key, data json );
 insert into People (data) values ( '{ "name": "michael", "Microsoft": "michaelchristophernewyork@hotmail.com", "email": "michaelchristophernewyork@hotmail.com", "Github": "mcny" } '::json);
+insert into People (data) values ( '{ "name": "Bill Gates", "Microsoft": "billg@microsoft.com", "email": "billg@microsoft.com" } '::json);
 select id, data from people;
