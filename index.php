eb43f1d978e0976d090efbd70abe1b33fedb9b7b
mcny <michaelchristophernewyork@hotmail.com>
2017-03-28 06:48:56 -0400

    make github upper case

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ $conn_string = "host=stampy.db.elephantsql.com port=5432 dbname=fzntsbqt user=fz
 $dbconn = \pg_connect($conn_string);
 //simple check
 $conn = \pg_connect($conn_string);
-$result = \pg_query($conn, "SELECT data->'name' AS name, data->'github' AS github, data->'email' AS email FROM People");
+$result = \pg_query($conn, "SELECT data->'name' AS name, data->'Github' AS github, data->'email' AS email FROM People");
 highlight_string("<?php\n\$data =\n" . var_export(\pg_fetch_all($result), true) . ";\n?>");
 
 echo "\r\n";
