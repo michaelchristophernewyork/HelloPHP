631317fdccf5b047643b70052ac3605bfa10287d
mcny <michaelchristophernewyork@hotmail.com>
2017-03-28 16:03:17 -0400

    remove direct connection to pgsql

--- index.php
+++ index.php
@@ -1,13 +1,4 @@
 <?php 
-$conn_string = "host=stampy.db.elephantsql.com port=5432 dbname=fzntsbqt user=fzntsbqt password=CtkEjRcQCIEQXh276-mINCY5h_g6yNUE";
-$dbconn = \pg_connect($conn_string);
-//simple check
-$conn = \pg_connect($conn_string);
-$result = \pg_query($conn, "SELECT data->'name' AS name, data->'Github' AS github, data->'email' AS email FROM People");
-highlight_string("<?php\n\$data =\n" . var_export(\pg_fetch_all($result), true) . ";\n?>");
-
-echo "\r\n";
-
 require_once('vendor/autoload.php'); 
 $user = \model\User::fetch_by_username('admin');
 echo $user->get_username();
