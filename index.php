b5161ce03e2b92695fd4f48000473a7c3f5a9628
mcny <michaelchristophernewyork@hotmail.com>
2017-03-29 07:11:29 -0400

    fix typo
    mode -> model

--- index.php
+++ index.php
@@ -2,5 +2,5 @@
 require_once('vendor/autoload.php'); 
 $user = \model\User::fetch_by_username('admin');
 echo $user->get_display_name();
-$user2 = \mode\User::fetch_by_email('kushaldeveloper@gmail.com');
+$user2 = \model\User::fetch_by_email('kushaldeveloper@gmail.com');
 if ($user2->verify_password('hunter2')) { echo 'huge success!'; } else { echo 'defeat!'; }
