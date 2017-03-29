1ba4b0daf17d082712e201b96ef839a96567626c
mcny <michaelchristophernewyork@hotmail.com>
2017-03-29 12:27:03 -0400

    add everything

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
 require_once('vendor/autoload.php'); 
-$user = \model\User::fetchByEmail('admin');
+$user = \model\User::fetchByUsername('admin');
 echo $user->getDisplayName();
 $user2 = \model\User::fetchByEmail('kushaldeveloper@gmail.com');
 if ($user2->verifyPassword('hunter2')) { echo 'huge success!'; } else { echo 'defeat!'; }
