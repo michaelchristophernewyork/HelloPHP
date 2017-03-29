cd297f6bb4689b3a3536acd702f35a29123a017c
mcny <michaelchristophernewyork@hotmail.com>
2017-03-29 11:54:04 -0400

    add everything

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
 require_once('vendor/autoload.php'); 
-$user = \model\User::fetch_by_username('admin');
-echo $user->get_display_name();
-$user2 = \model\User::fetch_by_email('kushaldeveloper@gmail.com');
-if ($user2->verify_password('hunter2')) { echo 'huge success!'; } else { echo 'defeat!'; }
+$user = \model\User::fetchByEmail('admin');
+echo $user->getDisplayName();
+$user2 = \model\User::fetchByEmail('kushaldeveloper@gmail.com');
+if ($user2->verifyPassword('hunter2')) { echo 'huge success!'; } else { echo 'defeat!'; }
