36529c8467320aa8617aaf6bd2bedec7c2111187
mcny <michaelchristophernewyork@hotmail.com>
2017-04-06 16:15:59 -0400

    add annoying new line
    echo '\r\n';

--- index.php
+++ index.php
@@ -1,12 +1,22 @@
-<?php 
-require_once('vendor/autoload.php'); 
+<?php
+require_once('vendor/autoload.php');
+echo '\r\n';
 $user = \model\User::fetchByUsername('admin');
+echo '\r\n';
 echo $user->getDisplayName();
+echo '\r\n';
 $user2 = \model\User::fetchByEmail('kushaldeveloper@gmail.com');
+echo '\r\n';
 if ($user2->verifyPassword('hunter2')) { echo 'huge success!'; } else { echo 'defeat!'; }
 
 if ($user->getUsername() == $user2->getUsername()) {
+    echo '\r\n';
     echo 'both users are the same';
+    echo '\r\n';
 } else {
     echo 'not the same at all';
+    echo '\r\n';
 }
+
+echo 'give them new line ';
+echo '\r\n';
