923eb59549f5633648d63bd4ebebf1cf3341e7c9
mcny <michaelchristophernewyork@hotmail.com>
2017-04-06 16:26:26 -0400

    switch to less annoying PHP_EOL
    echo '\r\n';

--- index.php
+++ index.php
@@ -1,22 +1,22 @@
 <?php
 require_once('vendor/autoload.php');
-echo '\r\n';
+echo PHP_EOL;
 $user = \model\User::fetchByUsername('admin');
-echo '\r\n';
+echo PHP_EOL;
 echo $user->getDisplayName();
-echo '\r\n';
+echo PHP_EOL;
 $user2 = \model\User::fetchByEmail('kushaldeveloper@gmail.com');
-echo '\r\n';
+echo PHP_EOL;
 if ($user2->verifyPassword('hunter2')) { echo 'huge success!'; } else { echo 'defeat!'; }
 
 if ($user->getUsername() == $user2->getUsername()) {
-    echo '\r\n';
+    echo PHP_EOL;
     echo 'both users are the same';
-    echo '\r\n';
+    echo PHP_EOL;
 } else {
     echo 'not the same at all';
-    echo '\r\n';
+    echo PHP_EOL;
 }
 
 echo 'give them new line ';
-echo '\r\n';
+echo PHP_EOL;
