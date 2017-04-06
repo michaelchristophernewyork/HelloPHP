93d5b0e2e5db3fbb68dd1e7995ef14b7b8ae10b9
mcny <michaelchristophernewyork@hotmail.com>
2017-04-06 10:46:26 -0400

    say both users are the same

--- index.php
+++ index.php
@@ -4,3 +4,7 @@ $user = \model\User::fetchByUsername('admin');
 echo $user->getDisplayName();
 $user2 = \model\User::fetchByEmail('kushaldeveloper@gmail.com');
 if ($user2->verifyPassword('hunter2')) { echo 'huge success!'; } else { echo 'defeat!'; }
+
+if ($user->getUsername() == $user2->getUsername()) {
+    echo 'both users are the same';
+}
