ebcd30d802629619d18f024c5e7418d6478cd658
mcny <michaelchristophernewyork@hotmail.com>
2017-04-09 10:51:52 -0400

    revert

--- index.php
+++ index.php
@@ -5,11 +5,10 @@ $loader = new Twig_Loader_Filesystem('templates');
 $twig = new Twig_Environment($loader);
 
 echo $twig->render('hello.html', array(
-    'a_variable' => 'Welcome to Twig!',
-    'navigation' => array(
-        'header' => array (
-            'href' => 'https://www.google.com',
-            'caption' => 'Google dot com'
-        )
+    'pageTitle' => 'Welcome to Penguins R Us!',
+    'products' => array(
+        'Tuxedo',
+        'Bow tie',
+        'Black Boxers',
     ),
 ));
