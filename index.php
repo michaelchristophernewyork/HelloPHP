4372cdcd2d26b402f05c0f1707b504ed8b4f1430
mcny <michaelchristophernewyork@hotmail.com>
2017-04-09 10:42:34 -0400

    remove declaration

--- index.php
+++ index.php
@@ -5,14 +5,6 @@ $loader = new Twig_Loader_Filesystem('templates');
 $twig = new Twig_Environment($loader);
 // setup some variables
 $pageTitle = 'Suit Up!';
-$products = array(
-    new Product('Serious Businessman', 'formal.png'),
-    new Product('Penguin Dress', 'dress.png'),
-    new Product('Sportstar Penguin', 'sports.png'),
-    new Product('Angel Costume', 'angel-costume.png'),
-    new Product('Penguin Accessories', 'swatter.png'),
-    new Product('Super Cool Penguin', 'super-cool.png'),
-);
 echo $twig->render('hello.html', array(
     'pageTitle' => 'Welcome to Penguins R Us!',
     'products' => array(
