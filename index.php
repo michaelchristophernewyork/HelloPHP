d8dd2a2c26d399802c09cd94afd5a665634a18ea
mcny <michaelchristophernewyork@hotmail.com>
2017-04-09 09:47:33 -0400

    use twig template
    https://twig.sensiolabs.org/doc/2.x/api.html

--- index.php
+++ index.php
@@ -1,20 +1,10 @@
 <?php
 require_once('vendor/autoload.php');
-$user = \model\User::fetchByUsername('admin');
-echo $user->getDisplayName() . PHP_EOL;
-$user2 = \model\User::fetchByEmail('kushaldeveloper@gmail.com');
-if ($user2->verifyPassword('hunter2')) {
-    echo 'huge success!' . PHP_EOL;
-} else {
-    echo 'defeat!' . PHP_EOL;
-}
 
-if ($user->getUsername() == $user2->getUsername()) {
-    echo 'both users are the same' . PHP_EOL;
-} else {
-    echo 'not the same at all' . PHP_EOL;
-}
+$loader = new Twig_Loader_Filesystem('templates');
+$twig = new Twig_Environment($loader, array(
+    'cache' => '/path/to/compilation_cache',
+));
 
-echo 'give them new line ' . PHP_EOL;
-
-echo 'fin';
+$template = $twig->load('index.html');
+echo $template->render(array('the' => 'variables', 'go' => 'here'));
