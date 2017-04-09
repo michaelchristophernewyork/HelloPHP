9e46e7faaaf3da92107ab18ed11afd68415fc0af
mcny <michaelchristophernewyork@hotmail.com>
2017-04-09 13:08:03 -0400

    play with twig some more

--- index.php
+++ index.php
@@ -3,12 +3,10 @@ require_once('vendor/autoload.php');
 
 $loader = new Twig_Loader_Filesystem('templates');
 $twig = new Twig_Environment($loader);
+$users = [];
+$user1 = \model\User::fetchByUsername('admin');
+array_push($users, $user1);
+$user2 = \model\User::fetchByUsername('admin');
+array_push($users, $user1);
 
-echo $twig->render('hello.html', array(
-    'pageTitle' => 'Welcome to Penguins R Us!',
-    'products' => array(
-        'Tuxedo',
-        'Bow tie',
-        'Black Boxers',
-    ),
-));
+echo $twig->render('hello.html', $users);
