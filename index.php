0d1099c8e35c2fc81245fceec7b7ce09a3c44de3
mcny <michaelchristophernewyork@hotmail.com>
2017-04-09 09:26:48 -0400

    use eol

--- index.php
+++ index.php
@@ -1,22 +1,20 @@
 <?php
 require_once('vendor/autoload.php');
-echo PHP_EOL;
 $user = \model\User::fetchByUsername('admin');
-echo PHP_EOL;
-echo $user->getDisplayName();
-echo PHP_EOL;
+echo $user->getDisplayName() . PHP_EOL;
 $user2 = \model\User::fetchByEmail('kushaldeveloper@gmail.com');
-echo PHP_EOL;
-if ($user2->verifyPassword('hunter2')) { echo 'huge success!'; } else { echo 'defeat!'; }
-
-if ($user->getUsername() == $user2->getUsername()) {
-    echo PHP_EOL;
-    echo 'both users are the same';
-    echo PHP_EOL;
+if ($user2->verifyPassword('hunter2')) {
+    echo 'huge success!' . PHP_EOL;
 } else {
-    echo 'not the same at all';
-    echo PHP_EOL;
+    echo 'defeat!' . PHP_EOL;
 }
 
-echo 'give them new line ';
-echo PHP_EOL;
+if ($user->getUsername() == $user2->getUsername()) {
+    echo 'both users are the same' . PHP_EOL;
+} else {
+    echo 'not the same at all' . PHP_EOL;
+}
+
+echo 'give them new line ' . PHP_EOL;
+
+echo 'fin';
