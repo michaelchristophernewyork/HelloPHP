af3eee7c6303063c2d02ff2ad138436cb5ebb216
mcny <michaelchristophernewyork@hotmail.com>
2017-04-09 15:42:20 -0400

    add another parameter

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once('vendor/autoload.php');
 $loader = new Twig_Loader_Filesystem('templates');
 $twig = new Twig_Environment($loader);
 $users = [];
+$pageTitle = 'hello, title';
 $user1 = \model\User::fetchByUsername('admin');
-array_push($users, $user1);
+array_push($users, $user, $pageTitle1);
 echo $twig->render('hello.html', $users);
