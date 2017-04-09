5da26c099359a34670c4f0380517a747a1ad798c
mcny <michaelchristophernewyork@hotmail.com>
2017-04-09 10:01:05 -0400

    remove cache

--- index.php
+++ index.php
@@ -2,9 +2,7 @@
 require_once('vendor/autoload.php');
 
 $loader = new Twig_Loader_Filesystem('templates');
-$twig = new Twig_Environment($loader, array(
-    'cache' => '',
-));
+$twig = new Twig_Environment($loader);
 
 $template = $twig->load('hello.html');
 echo $template->render(array('the' => 'variables', 'go' => 'here'));
