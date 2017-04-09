08022c54a39f1875828960cee2e6198c70dcd11d
mcny <michaelchristophernewyork@hotmail.com>
2017-04-09 09:57:51 -0400

    make chache blank
    will this help?

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ require_once('vendor/autoload.php');
 
 $loader = new Twig_Loader_Filesystem('templates');
 $twig = new Twig_Environment($loader, array(
-    'cache' => '/path/to/compilation_cache',
+    'cache' => '',
 ));
 
 $template = $twig->load('hello.html');
