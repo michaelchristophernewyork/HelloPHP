678a6b0312c0ac19fca6d42e470ed477054889f4
mcny <michaelchristophernewyork@hotmail.com>
2017-04-09 15:10:51 -0400

    get display name

--- index.php
+++ index.php
@@ -6,7 +6,5 @@ $twig = new Twig_Environment($loader);
 $users = [];
 $user1 = \model\User::fetchByUsername('admin');
 array_push($users, $user1);
-$user2 = \model\User::fetchByUsername('admin');
-array_push($users, $user1);
 
 echo $twig->render('hello.html', $users);
