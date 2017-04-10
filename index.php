1d8fc521c84799476aba43078b039ca24bd07dc1
mcny <michaelchristophernewyork@hotmail.com>
2017-04-10 08:22:43 -0400

    add another parameter

--- index.php
+++ index.php
@@ -1,11 +1,7 @@
 <?php
 require_once('vendor/autoload.php');
 
-$loader = new Twig_Loader_Filesystem('templates');
-$twig = new Twig_Environment($loader);
 $users = [];
 $user1 = \model\User::fetchByUsername('admin');
-array_push($users, $user1);
 $user2 = \model\User::addUser('kus', $user1->getDisplayName(), \model\User::hashPassword('hunter2'), 'billg@microsoft.com');
-array_push($users, $user2);
-echo $twig->render('hello.html', $users);
+echo $user2->verifyPassword('hunter2');
