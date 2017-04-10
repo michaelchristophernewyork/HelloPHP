a6e49c8cce36c5544de5fd421ea4566fa9d17aab
mcny <michaelchristophernewyork@hotmail.com>
2017-04-10 09:29:58 -0400

    remove more code

--- index.php
+++ index.php
@@ -2,4 +2,3 @@
 require_once('vendor/autoload.php');
 
 $user1 = \model\User::fetchByUsername('admin');
-$user2 = \model\User::addUser('kus', $user1->getDisplayName(), \model\User::hashPassword('hunter2'), 'billg@microsoft.com');
