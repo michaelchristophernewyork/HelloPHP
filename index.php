78a07c9bbe3400762f82ebd5772184df0015ec9b
mcny <michaelchristophernewyork@hotmail.com>
2017-04-10 09:22:33 -0400

    revert adding phpinfo
    why does it say admin?

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 require_once('vendor/autoload.php');
 
-$users = [];
 $user1 = \model\User::fetchByUsername('admin');
 $user2 = \model\User::addUser('kus', $user1->getDisplayName(), \model\User::hashPassword('hunter2'), 'billg@microsoft.com');
 echo $user2->verifyPassword('hunter2');
