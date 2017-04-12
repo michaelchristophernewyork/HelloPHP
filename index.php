78c74fb849f844bdb50181a93f31250018affa60
mc <michaelchristophernewyork@hotmail.com>
2017-04-12 14:58:10 -0400

    echo $settings["ldap_baseDN"];

--- index.php
+++ index.php
@@ -2,11 +2,4 @@
 require_once('vendor/autoload.php');
 $db = \model\Db::getInstance();
 $settings = \model\Setting::fetch_all();
-foreach ($settings as $setting)
-{
-    echo $setting;
-    // foreach ($setting as $value)
-    // {
-    //     echo $value . PHP_EOL;
-    // }
-}
+echo $settings["ldap_baseDN"];
