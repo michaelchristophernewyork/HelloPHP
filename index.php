a4a17146249c8d84eaf53a3a060eddf44ddcc9e5
mcny <michaelchristophernewyork@hotmail.com>
2017-04-12 01:36:50 -0400

    comment out user

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 require_once('vendor/autoload.php');
 $db = \model\Db::getInstance();
 $settings = \model\Setting::fetch_all();
-$user2 = new \model\User();
-$user2->addUserLdap('khada100', $settings);
+//$user2 = new \model\User();
+//$user2->addUserLdap('khada100', $settings);
 foreach ($settings as $setting) {
     echo $setting;
 }
