451f04646602a5429280db257251c54b48ebd313
mcny <michaelchristophernewyork@hotmail.com>
2017-04-12 01:42:53 -0400

    bring back user
    print name

--- index.php
+++ index.php
@@ -4,4 +4,4 @@ $db = \model\Db::getInstance();
 $settings = \model\Setting::fetch_all();
 $user2 = new \model\User();
 $user2->addUserLdap('khada100', $settings);
-$user2->getEmail();
+$user2->getUsername();
