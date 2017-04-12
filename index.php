f9a5341ba064183e7a70e45e4cefca27290bf0b8
mcny <michaelchristophernewyork@hotmail.com>
2017-04-12 01:32:08 -0400

    echo setting

--- index.php
+++ index.php
@@ -4,4 +4,6 @@ $db = \model\Db::getInstance();
 $settings = \model\Setting::fetch_all();
 $user2 = new \model\User();
 $user2->addUserLdap('khada100', $settings);
-echo $settings['remindermessage'];
+foreach ($settings as $setting) {
+    echo $setting;
+}
