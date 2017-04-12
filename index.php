45a4a445b8c8e75f5979b7d65493aa68d859acb7
mcny <michaelchristophernewyork@hotmail.com>
2017-04-12 01:39:55 -0400

    bring back user

--- index.php
+++ index.php
@@ -2,8 +2,5 @@
 require_once('vendor/autoload.php');
 $db = \model\Db::getInstance();
 $settings = \model\Setting::fetch_all();
-//$user2 = new \model\User();
-//$user2->addUserLdap('khada100', $settings);
-foreach ($settings as $setting) {
-    echo $setting;
-}
+$user2 = new \model\User();
+$user2->addUserLdap('khada100', $settings);
