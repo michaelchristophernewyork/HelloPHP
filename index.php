e9da53fc47d10c5f9e82357a183acab7a6255161
mc <michaelchristophernewyork@hotmail.com>
2017-04-12 09:53:08 -0400

    write php info

--- index.php
+++ index.php
@@ -2,6 +2,4 @@
 require_once('vendor/autoload.php');
 $db = \model\Db::getInstance();
 $settings = \model\Setting::fetch_all();
-$user2 = new \model\User();
-$user2->addUserLdap('khada100', $settings);
-$user2->getUsername();
+phpinfo();
