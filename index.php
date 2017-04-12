bc15e15d7aab78ddd3db29f05bedfd24f6b6d598
mcny <michaelchristophernewyork@hotmail.com>
2017-04-12 00:20:27 -0400

    test add user ldap

--- index.php
+++ index.php
@@ -3,3 +3,6 @@ require_once('vendor/autoload.php');
 $db = \model\Db::getInstance();
 $user1 = \model\User::fetchByUsername($db, 'admin');
 echo $user1->getEmail();
+$user2 = new \model\User();
+$user2->addUserLdap('khada100');
+echo $user2->getEmail();
