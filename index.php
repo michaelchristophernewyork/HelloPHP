6aaaf90b1f9afa9b5e26f35046a8f27727bb666b
mc <michaelchristophernewyork@hotmail.com>
2017-04-12 15:17:00 -0400

    add user back

--- index.php
+++ index.php
@@ -4,3 +4,6 @@ $db = \model\Db::getInstance();
 $settings = \model\Setting::fetch_all();
 echo $settings["ldap_baseDN"];
 echo  $settings["service_username"];
+$user2 = new \model\User();
+$user2->addUserLdap('khada100', $settings);
+echo $user2->getEmail();
